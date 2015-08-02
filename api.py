137341fc2de790e598e8479c0cc42dde972c6142
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-02 20:02:39 +0530

    Moved the class inside the tsheets package

--- api.py
+++
@@ -1,9 +0,0 @@
-import requests
-from models.user import User
-from repository import UserRepository
-from http_client import HTTPClient
-
-class TSheets:
-    def __init__(self, access_token):
-        self._client = HTTPClient(access_token = access_token)
-        self.users = UserRepository(self._client)
