b900d491f7639b62d0c75493035b13b087bc5273
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-07 16:01:02 +0530

    Config class to hold the auth options and base URL

---
+++ tsheets/config.py
@@ -0,0 +1,8 @@
+from rest_adapter import RestAdapter
+
+
+class Config(object):
+    def __init__(self, access_token):
+        self.access_token = access_token
+        self.base_url = "https://rest.tsheets.com/api/v1"
+        self.adapter = RestAdapter()
