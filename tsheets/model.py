67c45494b0cf659ce7ff014bdafca79073637709
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-07 16:03:11 +0530

    Model base class
    * Method to convert the raw json to the corresponding Model object

--- tsheets/model.py
+++ tsheets/model.py
@@ -1,20 +1,40 @@
 class Model(object):
-    def __init__(self):
-        self.accessors = {}
-        pass
+    accessors = {}
 
-    def add_field(self, fname, type_of_f):
+    def __init__(self, hash = None, options = {}):
+        self._dynamic_accessors = []
+        if hash:
+            self.__class__.mass_assign(self, hash, {})
+
+    def add_field(self, fname, type_f, options={}):
         setattr(self, fname, None)
-        self.accessors[fname] = {'name':fname, 'type': type_of_f}
+        if not self.__class__.accessors:
+            self.__class__.accessors[self.__class__] = []
+        exclude = options.get('exclude', [])
+        self.__class__.accessors[self.__class__].append({'name': fname, 'type': type_f, 'exclude': exclude})
 
     @classmethod
-    def from_raw(cls, json_response={}):
-        o = cls()
-        for k,v in json_response.items():
-            print k,v
-            if k in o.accessors:
-                print "yes"
-                setattr(o, k, v)
-                print o.email
-        return o
+    def from_raw(cls, hash):
+        instance = cls()
+        return cls.mass_assign(instance, hash)
+
+    @classmethod
+    def mass_assign(cls, instance, hash):
+        dynamic = instance._dynamic_accessors
+
+        for k,v in hash.iteritems():
+            casted = cls.cast_raw(v, k)
+            if hasattr(instance, k):
+                setattr(instance, k, casted)
+            else:
+                setattr(instance, k, casted)
+                dynamic.append({'name': k})
+        instance._dynamic_accessors = dynamic
+
+        return instance
+
+    @classmethod
+    def cast_raw(cls, value, key):
+
+
 
