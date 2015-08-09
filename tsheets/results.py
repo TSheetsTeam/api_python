5330493a139e5a0f005809c466ffcf017d949baa
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-09 23:59:22 +0530

    Fixed the iterator class by adding the Exception

--- tsheets/results.py
+++ tsheets/results.py
@@ -1,11 +1,11 @@
 import logger
 
 class Results(object):
-    def __init__(self, url, options, model, bridge, cache, is_singleton = False, mode = "list"):
+    def __init__(self, url, options, model, bridge, cache, is_singleton=False, mode="list"):
         self.url = url
         self.options = options
         self.model = model
-        self.name = "users"
+        self.name = "users" #todo
         self.index = -1
         self.loaded = []
         self.bridge = bridge
@@ -19,11 +19,13 @@ class Results(object):
 
     def next(self):
         if (self.index+1) < len(self.loaded) or (self.has_more and self.__load_next_batch()):
-            try:
-                self.index += 1
-                return self.loaded[self.index]
-            except:
-                raise StopIteration
+            self.index += 1
+            next_value = self.loaded[self.index]
+            print next_value
+            return next_value
+        else:
+            raise StopIteration
+
 
     def all(self):
         return list(self)
@@ -31,10 +33,12 @@ class Results(object):
     def __load_next_batch(self):
         response = self.bridge.next_batch(self.url, self.name, self.options, self.is_singleton, self.mode)
         batch = response['items']
+        print "Batch"
+        print batch
         self.has_more = not self.is_singleton and self.mode == 'list' and response['has_more']
         if self.is_singleton:
-          self.loaded = [ self.model.from_raw(batch, self.cache, response['supplemental']) ]
+          self.loaded = [ self.model.from_raw(batch) ]
         else:
-          self.loaded += [self.model.from_raw(o, self.cache, response['supplemental']) for o in batch ]
+          self.loaded += [self.model.from_raw(o) for o in batch ]
         return batch
 
