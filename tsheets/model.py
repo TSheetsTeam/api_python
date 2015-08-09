511cd6b1312a03847386384159df3cf0b0c58ca7
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-09 23:52:12 +0530

    Model Base Class to do the following
    1) JSON -> Python Object conversion
    2) Convert the unicode values to the actual Python datatypes
    3) Added the type check for those data types

--- tsheets/model.py
+++ tsheets/model.py
@@ -1,17 +1,24 @@
+import dateutil.parser
+import datetime
+from tsheets.helpers import to_class
+from tsheets.models import *
+
+
 class Model(object):
     accessors = {}
 
-    def __init__(self, hash = None, options = {}):
+    def __init__(self, hash = None):
         self._dynamic_accessors = []
         if hash:
-            self.__class__.mass_assign(self, hash, {})
+            self.__class__.mass_assign(self, hash)
 
-    def add_field(self, fname, type_f, options={}):
-        setattr(self, fname, None)
-        if not self.__class__.accessors:
-            self.__class__.accessors[self.__class__] = []
+    @classmethod
+    def add_field(cls, fname, type_f, options={}):
+        setattr(cls, fname, None)
+        if cls not in Model.accessors:
+            Model.accessors[cls] = []
         exclude = options.get('exclude', [])
-        self.__class__.accessors[self.__class__].append({'name': fname, 'type': type_f, 'exclude': exclude})
+        Model.accessors[cls].append({'name': fname, 'type': type_f, 'exclude': exclude})
 
     @classmethod
     def from_raw(cls, hash):
@@ -34,7 +41,55 @@ class Model(object):
         return instance
 
     @classmethod
-    def cast_raw(cls, value, key):
+    def type_for(cls, key):
+        for i in Model.accessors[cls]:
+            if i["name"] == key:
+                return i["type"]
+        return "anything"
 
+    @classmethod
+    def type_for_key(cls, key):
+        return cls.type_for(key)
 
+    @classmethod
+    def cast_raw(cls, value, key, type=None):
+        if not value:
+            return None
 
+        if type:
+            type_symbol = type
+        else:
+            type_symbol = cls.type_for_key(key)
+
+        if isinstance(type_symbol, list):
+            value = [cls.cast_raw(i, key, type_symbol[0]) for i in value]
+            return value
+        elif type_symbol == "str":
+            return value
+        elif type_symbol == "int":
+            return int(value)
+        elif type_symbol == "datetime":
+            try:
+                return dateutil.parser.parse(value)
+            except:
+                return None
+        elif type_symbol == "date":
+            try:
+                return datetime.datetime.strptime(value, "%Y-%m-%d").date()
+            except:
+                return None
+        elif type_symbol == "bool":
+            return value == True
+        elif type_symbol == "dict":
+            return value
+        elif type_symbol == "float":
+            return float(value)
+        elif type_symbol == "object":
+            if not value:
+                return {}
+            return value
+        elif type_symbol == "anything":
+            return value
+        else:
+            #to_class(type_symbol)().from_raw(value)
+            pass
