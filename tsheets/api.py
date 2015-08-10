8ba3824fed77eea07416574589e2e17d9460ceca
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-11 02:47:39 +0530

    Initialize all the repository classes and set to api instance

--- tsheets/api.py
+++ tsheets/api.py
@@ -1,17 +1,20 @@
 import requests
-from repos import Users
+from tsheets.repository import Repository
 from config import Config
 from bridge import Bridge
-import logger
+from helpers import class_to_endpoint
 
 
 class TSheets:
-    repos = [{"name": "users", "class": Users}]
+    repos = []
+    for repo in Repository.inherited_classes:
+        name = class_to_endpoint(repo.__name__)
+        repos.append({"name": name, "class": repo})
 
     def __init__(self, access_token):
         self.config = Config(access_token)
         self.bridge = Bridge(self.config)
         self.cache = None
         for repo in TSheets.repos:
+            print repo["name"]
             setattr(self, repo["name"], repo["class"](self.bridge))
-
