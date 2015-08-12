663b9c39668366b74e120c3a008bb06b088b919e
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-12 15:20:09 +0530

    Convert datetime string to isoformat
    * Get the string from the user, and parse it first to see if it in
    date/datetime format
    * If not throw an exception
    * If it has correct value, then convert that into iso8601 formatted
    datetime with timezone aware config

--- tsheets/repository.py
+++ tsheets/repository.py
@@ -3,6 +3,7 @@ from results import Results
 import dateutil
 from error import FilterInvalidValueError
 from datetime import date, datetime
+import pytz
 
 
 class Repository(object):
@@ -18,34 +19,33 @@ class Repository(object):
 
     def validated_options(self, options):
         for name, value in options.iteritems():
-            self.validate_options(name, value)
-        return options
-
-    def validate_options(self, name, value):
-        if name in Repository.filters:
-            type_of_filter = Repository.filters[name]
-            if isinstance(type_of_filter, list):
-                if not isinstance(value, list):
-                    raise FilterInvalidValueError("Expected the value of {} filter to be an list".format(name))
+            if name in Repository.filters:
+                type_of_filter = Repository.filters[name]
+                if isinstance(type_of_filter, list):
+                    if not isinstance(value, list):
+                        raise FilterInvalidValueError("Expected the value of {} filter to be an list".format(name))
+                    else:
+                        for i in value:
+                            if not isinstance(i, type_of_filter[0]):
+                                raise FilterInvalidValueError("Expected all the values of a list for the {} filter to "
+                                                              "match the given type {}".format(name, type_of_filter))
                 else:
-                    for i in value:
-                        if not isinstance(i, type_of_filter[0]):
-                            raise FilterInvalidValueError("Expected all the values of a list for the {} filter to "
-                                                          "match the given type {}".format(name, type_of_filter))
+                    if type_of_filter == datetime or type_of_filter == date:
+                        dt = self.validate_datetime(value, type_of_filter)
+                        options[name] = str(dt)
+                    if not isinstance(value, type_of_filter):
+                        raise FilterInvalidValueError("Expected the value for the {} filter to "
+                                                      "match the given type: {}".format(name, type_of_filter))
             else:
-                if type_of_filter == datetime or type_of_filter == date:
-                    self.validate_datetime(value, type_of_filter)
-                    return
-                if not isinstance(value, type_of_filter):
-                    raise FilterInvalidValueError("Expected the value for the {} filter to "
-                                                  "match the given type: {}".format(name, type_of_filter))
-        else:
-            raise FilterInvalidValueError("Unknown Filter for class - {} filter - {}".format(self.__class__, name))
+                raise FilterInvalidValueError("Unknown Filter for class - {} filter - {}".format(self.__class__, name))
 
     def validate_datetime(self, value, type_of_filter):
         try:
-            dateutil.parser.parse(value)
-            return
+            value_with_tz = ""
+            dt = dateutil.parser.parse(value)
+            if not dt.tzinfo:
+               value_with_tz = value.replace(tzinfo=pytz.UTC)
+            return value_with_tz
         except:
             if type_of_filter == "datetime":
                 raise FilterInvalidValueError("Expected datetime is String (ISO8601 format) "
