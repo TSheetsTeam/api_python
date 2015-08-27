239e9a9f5dfb851f223d0ba9277f8bc1bf4e6ef0
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-27 11:42:02 +0530

    Excluded few fields from edit/add operations

--- tsheets/models/timesheet.py
+++ tsheets/models/timesheet.py
@@ -6,14 +6,14 @@ class Timesheet(Model):
     pass
 
 
-Timesheet.add_field("id", int, {"exclude": ['add', 'edit']})
+Timesheet.add_field("id", int, {"exclude": ['add']})
 Timesheet.add_field("user_id", int)
 Timesheet.add_field("jobcode_id", int)
 Timesheet.add_field("locked", int, {"exclude": ['add', 'edit']})
 Timesheet.add_field("notes", str)
 Timesheet.add_field("customfields", str)
-Timesheet.add_field("created", datetime)
-Timesheet.add_field("last_modified", datetime)
+Timesheet.add_field("created", datetime, {"exclude": ['add', 'edit']})
+Timesheet.add_field("last_modified", datetime, {"exclude": ['add', 'edit']})
 Timesheet.add_field("type", str)
 Timesheet.add_field("on_the_clock", bool, {"exclude": ['add', 'edit']})
 Timesheet.add_field("start", datetime)
