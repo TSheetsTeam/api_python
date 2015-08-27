fcf0d50d6ecb762fe7369dae745aba58da01617e
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-27 11:40:30 +0530

    Added the method to convert class name to class object

--- tsheets/helpers.py
+++ tsheets/helpers.py
@@ -1,10 +1,27 @@
 import re
+from tsheets.models import *
+import sys
+import types
+import tsheets
+import importlib
+print globals()
+
+
+def get_class(kls):
+    parts = kls.split('.')
+    module = ".".join(parts[:-1])
+    m = __import__( module )
+    for comp in parts[1:]:
+        m = getattr(m, comp)
+    return m
 
 def to_class(value):
     components = value.split('_')
     # We capitalize the first letter of each component
     # with the 'title' method and join them together.
-    return "".join(x.title() for x in components)
+    field = "".join(x.title() for x in components)
+    field = "tsheets.models.{}.{}".format(value, field)
+    return get_class(field)
 
 
 def class_to_endpoint(name):
