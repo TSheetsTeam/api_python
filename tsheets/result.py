05bbc7669162f1ba7752ad44cd0d8e93ff5060b9
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-27 11:27:08 +0530

    Added Result class
    The instance of Result class will be the response for the
    insert/update/delete operations

---
+++ tsheets/result.py
@@ -0,0 +1,23 @@
+class Result(object):
+
+    def __init__(self, code, body):
+        self.code = code
+        self.body = body
+
+    def is_success(self):
+        return self.code == 200 and self._get_status_code() == 200
+
+    def _get_status_code(self):
+        try:
+            status_code = self.body['results'].values()[0].values()[0]['_status_code']
+            print status_code
+            return status_code
+        except:
+            return 0
+
+    def message(self):
+        try:
+            message = self.body['results'].values()[0].values()[0]['_status_extra']
+            return "Unexpected API response - inspect result body. Exception Message: {}".format(message)
+        except:
+            return ""
