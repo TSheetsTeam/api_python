78187164066422424500ca8835881174489a8e0c
Kannan Ponnusamy <kannan@endpoint.com>
2015-08-31 18:18:52 +0530

    Fixed the tarball url

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-    name = 'tsheets',
-    packages = ['tsheets'],
-    version = '0.1',
-    description = 'API library helper for TSheets.com',
-    long_description= 'Allows to use the TSheets.com API to manage the timesheets and all other related data',
-    author = 'Kannan Ponnusamy',
-    author_email = 'kannan@endpoint.com',
+    name='tsheets',
+    packages=['tsheets'],
+    version='0.1',
+    description='API library helper for TSheets.com',
+    long_description='Allows to use the TSheets.com API to manage the timesheets and all other related data',
+    author='Kannan Ponnusamy',
+    author_email ='kannan@endpoint.com',
     license='MIT',
-    url = 'https://github.com/tsheets/api_python',
-    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-    keywords = ['api', 'rest', 'tsheets'],
+    url='https://github.com/tsheets/api_python',
+    download_url='https://github.com/tsheets/api_python/tarball/0.1',
+    keywords=['api', 'rest', 'tsheets'],
     install_requires=['requests>=2.7.0']
 )
